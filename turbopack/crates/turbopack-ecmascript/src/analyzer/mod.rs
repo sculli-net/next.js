007ac5b73b382d37d6d54499c95a06a075755676
Luke Sandberg <lukeisandberg@gmail.com>
2025-08-21 12:10:17 -0700

    improve naming

--- turbopack/crates/turbopack-ecmascript/src/analyzer/mod.rs
+++ turbopack/crates/turbopack-ecmascript/src/analyzer/mod.rs
@@ -3808,7 +3808,7 @@ mod tests {
         linker::link,
     };
     use crate::analyzer::{
-        graph::{AssignmentKinds, VarMeta},
+        graph::{AssignmentScopes, VarMeta},
         imports::ImportAttributes,
     };
 
@@ -3870,13 +3870,17 @@ mod tests {
                 named_values.sort_by(|a, b| a.0.cmp(&b.0));
 
                 fn explain_all<'a>(
-                    values: impl IntoIterator<Item = (&'a String, &'a JsValue, Option<AssignmentKinds>)>,
+                    values: impl IntoIterator<
+                        Item = (&'a String, &'a JsValue, Option<AssignmentScopes>),
+                    >,
                 ) -> String {
                     values
                         .into_iter()
-                        .map(|(id, value, assignment_kinds)| {
-                            let non_root_assignments = match assignment_kinds {
-                                Some(AssignmentKinds::AllInRootScope) => " (const after eval)",
+                        .map(|(id, value, assignment_scopes)| {
+                            let non_root_assignments = match assignment_scopes {
+                                Some(AssignmentScopes::AllInModuleEvalScope) => {
+                                    " (const after eval)"
+                                }
                                 _ => "",
                             };
                             let (explainer, hints) = value.explain(10, 5);
@@ -3896,7 +3900,7 @@ mod tests {
                             "{:#?}",
                             named_values
                                 .iter()
-                                .map(|(name, (_, VarMeta {value, ..}))| (name, value))
+                                .map(|(name, (_, VarMeta { value, .. }))| (name, value))
                                 .collect::<Vec<_>>()
                         ))
                         .compare_to_file(&graph_snapshot_path)
@@ -3909,10 +3913,10 @@ mod tests {
                                 _,
                                 VarMeta {
                                     value,
-                                    assignment_kinds,
+                                    assignment_scopes,
                                 },
                             ),
-                        )| (name, value, Some(*assignment_kinds)),
+                        )| (name, value, Some(*assignment_scopes)),
                     )))
                     .compare_to_file(&graph_explained_snapshot_path)
                     .unwrap();

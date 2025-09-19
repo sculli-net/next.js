e362aea10df4a3b6d8f3c14488ff49aa417867ce
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-09-19 21:25:46 +0200

    Turbopack: correctly catch all issues for builds

--- packages/next/src/build/swc/types.ts
+++ packages/next/src/build/swc/types.ts
@@ -222,7 +222,7 @@ export interface Project {
 
   writeAllEntrypointsToDisk(
     appDirOnly: boolean
-  ): Promise<TurbopackResult<RawEntrypoints>>
+  ): Promise<TurbopackResult<RawEntrypoints | {}>>
 
   entrypointsSubscribe(): AsyncIterableIterator<TurbopackResult<RawEntrypoints>>
 
